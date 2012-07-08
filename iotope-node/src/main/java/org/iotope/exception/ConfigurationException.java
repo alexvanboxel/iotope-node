f27ed97f7082b39df6eb9734173da4fcc573965f
witspirit <witspirit@gmail.com>
2012-07-08 22:25:41 +0200

    Ensured the application terminates if the configuration could not be loaded.
    * Introduced new Exceptions: IotopeException, ConfigirationException
    * Refactored Configuration
    *- Extracted path determination
    *- Use of the new Exceptions
    * Adapted the main to handle the exception properly
    *- For now, system.exit, since no clean shutdown procedure is available
    * Removed some commented out code

---
+++ iotope-node/src/main/java/org/iotope/exception/ConfigurationException.java
@@ -0,0 +1,22 @@
+package org.iotope.exception;
+
+
+public class ConfigurationException extends IotopeException {
+
+    public ConfigurationException() {
+        super();
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(Throwable cause) {
+        super(cause);
+    }
+    
+}
